da175b25e7b4490bab049ac01006846551527015
Facundo Prieto <facundo.prieto321@gmail.com>
2021-12-08 20:47:37 -0300

    Create workspace CollapsableItem

---
+++ app/workspaces/components/WorkspaceCollapsableItem.tsx
@@ -0,0 +1,28 @@
+import { Avatar, Collapse, Stack, Tooltip, useDisclosure } from "@chakra-ui/react"
+import { useQuery } from "blitz"
+import getWorkspace from "../queries/getWorkspace"
+
+const Item = ({ name, onClick }) => (
+  <Tooltip label={name} placement="auto">
+    <Avatar name={name} onClick={onClick} />
+  </Tooltip>
+)
+
+export default function WorkspaceCollapsableItem({ workspaceId }) {
+  const [workspace] = useQuery(getWorkspace, { id: workspaceId })
+
+  const { isOpen, onToggle } = useDisclosure()
+
+  return (
+    <Stack w="min-content" bg="gray" borderRadius={"full"}>
+      <Item name={workspace?.name} onClick={onToggle} />
+      <Collapse in={isOpen} animateOpacity>
+        <Stack>
+          {workspace?.projects?.map((project) => (
+            <Item key={project?.id} name={project?.name} onClick={onToggle} />
+          ))}
+        </Stack>
+      </Collapse>
+    </Stack>
+  )
+}
