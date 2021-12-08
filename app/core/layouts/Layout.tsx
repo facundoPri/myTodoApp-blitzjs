90d262136a2ba1d6bfc690b919a76a6127a0b47f
Facundo Prieto <facundo.prieto321@gmail.com>
2021-12-08 19:31:10 -0300

    Use chakra ui on main layout

--- app/core/layouts/Layout.tsx
+++ app/core/layouts/Layout.tsx
@@ -1,14 +1,19 @@
+import { Flex, useColorModeValue } from "@chakra-ui/react"
 import { Head, BlitzLayout } from "blitz"
 
-const Layout: BlitzLayout<{title?: string}> = ({ title, children }) => {
+const Layout: BlitzLayout<{ title?: string }> = ({ title, children, ...rest }) => {
+  const bg = useColorModeValue("white", "black")
+  const color = useColorModeValue("black", "white")
+
   return (
     <>
       <Head>
         <title>{title || "myTodoApp"}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      {children}
+      <Flex direction="column" bg={bg} color={color} minH="100vh" {...rest}>
+        {children}
+      </Flex>
     </>
   )
 }
