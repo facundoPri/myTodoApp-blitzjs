f6dc5cde71a2885e99749b653e0a20d7e1f5390e
Facundo Prieto <facundo.prieto321@gmail.com>
2021-12-09 16:06:42 -0300

    Some changes on workspaceCollapsableItem
    Make projects smallers and add AddButton

--- app/workspaces/components/WorkspaceCollapsableItem.tsx
+++ app/workspaces/components/WorkspaceCollapsableItem.tsx
@@ -1,11 +1,26 @@
-import { Avatar, Collapse, SkeletonCircle, Stack, Tooltip, useDisclosure } from "@chakra-ui/react"
+import { AddIcon } from "@chakra-ui/icons"
+import {
+  Avatar,
+  Collapse,
+  IconButton,
+  SkeletonCircle,
+  Stack,
+  Tooltip,
+  useDisclosure,
+} from "@chakra-ui/react"
 import { Link, Routes, useQuery } from "blitz"
 import { useEffect } from "react"
 import getWorkspace from "../queries/getWorkspace"
 
-const Item = ({ name, onClick, isActive = false }) => (
+const Item = ({ name, onClick, isActive = false, ...rest }) => (
   <Tooltip label={name} placement="auto">
-    <Avatar name={name} onClick={onClick} cursor={"pointer"} border={isActive ? "2px solid" : ""} />
+    <Avatar
+      name={name}
+      onClick={onClick}
+      cursor={"pointer"}
+      border={isActive ? "2px solid" : ""}
+      {...rest}
+    />
   </Tooltip>
 )
 
@@ -26,15 +41,25 @@ export default function WorkspaceCollapsableItem({ workspaceId, isLink, isActive
         <Item name={workspace?.name} onClick={onToggle} isActive={isActive} />
       )}
       <Collapse in={isOpen} animateOpacity>
-        <Stack>
+        <Stack alignItems={"center"}>
           {workspace?.projects?.map((project) => (
             <Link
               key={project?.id}
               href={Routes.ShowProjectPage({ workspaceId: workspaceId, projectId: project.id })}
             >
-              <Item name={project?.name} onClick={onToggle} />
+              <Item name={project?.name} onClick={onToggle} boxSize="10" />
             </Link>
           ))}
+
+          <Link href={Routes.NewProjectPage({ workspaceId })}>
+            <IconButton
+              boxSize={10}
+              bg="gray.400"
+              aria-label="Add Workspace"
+              icon={<AddIcon boxSize={5} />}
+              borderRadius={"full"}
+            />
+          </Link>
         </Stack>
       </Collapse>
     </Stack>
