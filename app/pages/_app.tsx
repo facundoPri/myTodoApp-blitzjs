311bf2e3a26d4ebbf2b79bda95ca98295945d200
Facundo Prieto <facundo.prieto321@gmail.com>
2021-12-09 19:15:46 -0300

    Add head on document

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -21,23 +21,6 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <DefaultSeo defaultTitle={`${APP_NAME}`} titleTemplate={`%s | ${APP_NAME}`} />
-      <Head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1, viewport-fit=cover, user-scalable=no" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <meta name="msapplication-TileColor" content="#6236ff" />
-        <meta name="msapplication-TileImage" content="/mstile-144x144.png" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
       <ChakraProvider resetCSS theme={theme}>
         <ErrorBoundary
           FallbackComponent={RootErrorFallback}
--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -12,7 +12,23 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <DocumentHead />
+        <DocumentHead>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="initial-scale=1, viewport-fit=cover, user-scalable=no" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <meta name="msapplication-TileColor" content="#6236ff" />
+          <meta name="msapplication-TileImage" content="/mstile-144x144.png" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+          />
+        </DocumentHead>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
