b4044b64ddbe4150d90f84f852976b7d971804eb
Facundo Prieto <facundo.prieto321@gmail.com>
2021-12-09 16:05:20 -0300

    Connect current with workspace on creation

--- app/workspaces/mutations/createWorkspace.ts
+++ app/workspaces/mutations/createWorkspace.ts
@@ -6,9 +6,17 @@ const CreateWorkspace = z.object({
   name: z.string(),
 })
 
-export default resolver.pipe(resolver.zod(CreateWorkspace), resolver.authorize(), async (input) => {
-  // TODO: in multi-tenant app, you must add validation to ensure correct tenant
-  const workspace = await db.workspace.create({ data: input })
+export default resolver.pipe(
+  resolver.zod(CreateWorkspace),
+  resolver.authorize(),
+  async (input, ctx) => {
+    // TODO: in multi-tenant app, you must add validation to ensure correct tenant
+    const data = {
+      ...input,
+      ownerId: ctx.session.userId,
+    }
+    const workspace = await db.workspace.create({ data: data })
 
-  return workspace
-})
+    return workspace
+  }
+)
