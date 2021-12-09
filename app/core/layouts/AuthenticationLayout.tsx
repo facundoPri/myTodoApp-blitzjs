af167536b383ef9916d4295c2e26261bc38bb3cb
Facundo Prieto <facundo.prieto321@gmail.com>
2021-12-09 19:05:16 -0300

    Update Layouts

--- app/core/layouts/AuthenticationLayout.tsx
+++ app/core/layouts/AuthenticationLayout.tsx
@@ -1,5 +1,6 @@
 import { Stack, useColorModeValue } from "@chakra-ui/react"
 import { Head, BlitzLayout } from "blitz"
+import { NextSeo } from "next-seo"
 
 const AuthenticationLayout: BlitzLayout<{ title?: string }> = ({ title, children, ...rest }) => {
   const bg = useColorModeValue("white", "black")
@@ -7,11 +8,8 @@ const AuthenticationLayout: BlitzLayout<{ title?: string }> = ({ title, children
 
   return (
     <>
-      <Head>
-        <title>{title || "myTodoApp"}</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Stack bg={bg} color={color} minH="100vh" m="auto" maxW="500px" mt="20" {...rest}>
+      <NextSeo title={title} />
+      <Stack bg={bg} color={color} minH="100vh" m="auto" {...rest}>
         {children}
       </Stack>
     </>
--- app/core/layouts/Layout.tsx
+++ app/core/layouts/Layout.tsx
@@ -1,6 +1,7 @@
 import { Flex, Stack, useColorModeValue } from "@chakra-ui/react"
 import { Head, BlitzLayout } from "blitz"
 import SideBar from "../components/SideBar"
+import { NextSeo } from "next-seo"
 
 const Layout: BlitzLayout<{ title?: string }> = ({ title, children, ...rest }) => {
   const bg = useColorModeValue("white", "black")
@@ -8,10 +9,7 @@ const Layout: BlitzLayout<{ title?: string }> = ({ title, children, ...rest }) =
 
   return (
     <>
-      <Head>
-        <title>{title || "myTodoApp"}</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <NextSeo title={title} />
       <Flex bg={bg} color={color}>
         <SideBar />
         <Stack bg={bg} color={color} minH="100vh" {...rest}>
