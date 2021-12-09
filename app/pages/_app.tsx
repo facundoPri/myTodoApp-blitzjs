3d21221b5a9d323740ce966b742ff0a4fe19eed8
Facundo Prieto <facundo.prieto321@gmail.com>
2021-12-09 19:03:43 -0300

    Some changes on app and document pages

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -4,6 +4,7 @@ import {
   ErrorComponent,
   AuthenticationError,
   AuthorizationError,
+  Head,
   ErrorFallbackProps,
   useQueryErrorResetBoundary,
 } from "blitz"
@@ -11,19 +12,41 @@ import LoginForm from "app/auth/components/LoginForm"
 
 import { ChakraProvider } from "@chakra-ui/react"
 import theme from "../core/theme"
+import { APP_NAME } from "app/config"
+import { DefaultSeo } from "next-seo"
 
 export default function App({ Component, pageProps }: AppProps) {
   const getLayout = Component.getLayout || ((page) => page)
 
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <ErrorBoundary
-        FallbackComponent={RootErrorFallback}
-        onReset={useQueryErrorResetBoundary().reset}
-      >
-        {getLayout(<Component {...pageProps} />)}
-      </ErrorBoundary>
-    </ChakraProvider>
+    <>
+      <DefaultSeo defaultTitle={`${APP_NAME}`} titleTemplate={`%s | ${APP_NAME}`} />
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1, viewport-fit=cover, user-scalable=no" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#6236ff" />
+        <meta name="msapplication-TileImage" content="/mstile-144x144.png" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <ChakraProvider resetCSS theme={theme}>
+        <ErrorBoundary
+          FallbackComponent={RootErrorFallback}
+          onReset={useQueryErrorResetBoundary().reset}
+        >
+          {getLayout(<Component {...pageProps} />)}
+        </ErrorBoundary>
+      </ChakraProvider>
+    </>
   )
 }
 
