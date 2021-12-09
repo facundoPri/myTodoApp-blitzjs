180a6679159fcb7c8d5a6ef03fd27ef1fbf41150
Facundo Prieto <facundo.prieto321@gmail.com>
2021-12-09 20:24:12 -0300

    Show tasks on get project

--- app/projects/queries/getProject.ts
+++ app/projects/queries/getProject.ts
@@ -9,7 +9,7 @@ const GetProject = z.object({
 
 export default resolver.pipe(resolver.zod(GetProject), resolver.authorize(), async ({ id }) => {
   // TODO: in multi-tenant app, you must add validation to ensure correct tenant
-  const project = await db.project.findFirst({ where: { id } })
+  const project = await db.project.findFirst({ where: { id }, include: { tasks: true } })
 
   if (!project) throw new NotFoundError()
 
