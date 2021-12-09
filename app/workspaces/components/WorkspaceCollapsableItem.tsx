d52fcc5d0e883bf422040b966ab9613df826d0e0
Facundo Prieto <facundo.prieto321@gmail.com>
2021-12-09 00:46:58 -0300

    Changes on workspaceCollapsableItems

--- app/workspaces/components/WorkspaceCollapsableItem.tsx
+++ app/workspaces/components/WorkspaceCollapsableItem.tsx
@@ -1,25 +1,39 @@
-import { Avatar, Collapse, Stack, Tooltip, useDisclosure } from "@chakra-ui/react"
-import { useQuery } from "blitz"
+import { Avatar, Collapse, SkeletonCircle, Stack, Tooltip, useDisclosure } from "@chakra-ui/react"
+import { Link, Routes, useQuery } from "blitz"
+import { useEffect } from "react"
 import getWorkspace from "../queries/getWorkspace"
 
-const Item = ({ name, onClick }) => (
+const Item = ({ name, onClick, isActive = false }) => (
   <Tooltip label={name} placement="auto">
-    <Avatar name={name} onClick={onClick} cursor={"pointer"} />
+    <Avatar name={name} onClick={onClick} cursor={"pointer"} border={isActive ? "2px solid" : ""} />
   </Tooltip>
 )
 
-export default function WorkspaceCollapsableItem({ workspaceId }) {
-  const [workspace] = useQuery(getWorkspace, { id: workspaceId })
+export default function WorkspaceCollapsableItem({ workspaceId, isLink, isActive }) {
+  const [workspace] = useQuery(getWorkspace, { id: workspaceId }, { suspense: false })
 
-  const { isOpen, onToggle } = useDisclosure()
+  const { isOpen, onToggle } = useDisclosure({ isOpen: isActive })
+
+  if (!workspace) return <SkeletonCircle size="12" />
 
   return (
-    <Stack w="min-content" bg="gray" borderRadius={"full"}>
-      <Item name={workspace?.name} onClick={onToggle} />
+    <Stack w="min-content" bg="gray.600" borderRadius={"full"}>
+      {isLink ? (
+        <Link href={Routes.ShowWorkspacePage({ workspaceId: workspaceId })}>
+          <Item name={workspace?.name} onClick={onToggle} isActive={isActive} />
+        </Link>
+      ) : (
+        <Item name={workspace?.name} onClick={onToggle} isActive={isActive} />
+      )}
       <Collapse in={isOpen} animateOpacity>
         <Stack>
           {workspace?.projects?.map((project) => (
-            <Item key={project?.id} name={project?.name} onClick={onToggle} />
+            <Link
+              key={project?.id}
+              href={Routes.ShowProjectPage({ workspaceId: workspaceId, projectId: project.id })}
+            >
+              <Item name={project?.name} onClick={onToggle} />
+            </Link>
           ))}
         </Stack>
       </Collapse>
