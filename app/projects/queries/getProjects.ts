ff6843bc1b6b96eea52300524236e4d6d9c066bc
Facundo Prieto <facundo.prieto321@gmail.com>
2021-12-10 14:14:40 -0300

    Remove pagination from getProjects

--- app/projects/queries/getProjects.ts
+++ app/projects/queries/getProjects.ts
@@ -1,30 +1,16 @@
-import { paginate, resolver } from "blitz"
+import { NotFoundError, resolver } from "blitz"
 import db, { Prisma } from "db"
 
-interface GetProjectsInput
-  extends Pick<Prisma.ProjectFindManyArgs, "where" | "orderBy" | "skip" | "take"> {}
+interface GetProjectsInput extends Pick<Prisma.ProjectFindManyArgs, "where" | "orderBy"> {}
 
-export default resolver.pipe(
-  resolver.authorize(),
-  async ({ where, orderBy, skip = 0, take = 100 }: GetProjectsInput) => {
-    // TODO: in multi-tenant app, you must add validation to ensure correct tenant
-    const {
-      items: projects,
-      hasMore,
-      nextPage,
-      count,
-    } = await paginate({
-      skip,
-      take,
-      count: () => db.project.count({ where }),
-      query: (paginateArgs) => db.project.findMany({ ...paginateArgs, where, orderBy }),
-    })
+export default resolver.pipe(resolver.authorize(), async ({ where, orderBy }: GetProjectsInput) => {
+  // TODO: in multi-tenant app, you must add validation to ensure correct tenant
+  const projects = db.project.findMany({ where, orderBy })
 
-    return {
-      projects,
-      nextPage,
-      hasMore,
-      count,
-    }
-  }
-)
+  // const workspaces = await db.workspace.findMany({
+  //   where: { ownerId: ctx.session.userId, ...where },
+  //   orderBy,
+  // })
+
+  return projects
+})
