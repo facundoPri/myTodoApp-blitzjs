740f33f7e6ce6e0539ef2e70d234b83308930160
Facundo Prieto <facundo.prieto321@gmail.com>
2021-12-10 02:33:03 -0300

    Update getWorkspaces

--- app/workspaces/queries/getWorkspaces.ts
+++ app/workspaces/queries/getWorkspaces.ts
@@ -6,25 +6,13 @@ interface GetWorkspacesInput
 
 export default resolver.pipe(
   resolver.authorize(),
-  async ({ where, orderBy, skip = 0, take = 100 }: GetWorkspacesInput) => {
+  async ({ where, orderBy }: GetWorkspacesInput, ctx) => {
     // TODO: in multi-tenant app, you must add validation to ensure correct tenant
-    const {
-      items: workspaces,
-      hasMore,
-      nextPage,
-      count,
-    } = await paginate({
-      skip,
-      take,
-      count: () => db.workspace.count({ where }),
-      query: (paginateArgs) => db.workspace.findMany({ ...paginateArgs, where, orderBy }),
+    const workspaces = await db.workspace.findMany({
+      where: { ownerId: ctx.session.userId, ...where },
+      orderBy,
     })
 
-    return {
-      workspaces,
-      nextPage,
-      hasMore,
-      count,
-    }
+    return workspaces
   }
 )
