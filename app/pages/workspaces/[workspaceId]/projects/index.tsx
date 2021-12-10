f686856be490f70b4adc61d49be2e4e52707dce5
Facundo Prieto <facundo.prieto321@gmail.com>
2021-12-10 16:23:58 -0300

    Remove pagintaion from from projects index page

--- app/pages/workspaces/[workspaceId]/projects/index.tsx
+++ app/pages/workspaces/[workspaceId]/projects/index.tsx
@@ -1,23 +1,22 @@
 import { Suspense } from "react"
-import { Head, Link, usePaginatedQuery, useRouter, useParam, BlitzPage, Routes } from "blitz"
+import {
+  Head,
+  Link,
+  usePaginatedQuery,
+  useRouter,
+  useParam,
+  BlitzPage,
+  Routes,
+  useQuery,
+} from "blitz"
 import Layout from "app/core/layouts/Layout"
 import getProjects from "app/projects/queries/getProjects"
 
-const ITEMS_PER_PAGE = 100
-
 export const ProjectsList = () => {
-  const router = useRouter()
-  const page = Number(router.query.page) || 0
   const workspaceId = useParam("workspaceId", "number")
-  const [{ projects, hasMore }] = usePaginatedQuery(getProjects, {
+  const [projects] = useQuery(getProjects, {
     where: { workspace: { id: workspaceId! } },
-    orderBy: { id: "asc" },
-    skip: ITEMS_PER_PAGE * page,
-    take: ITEMS_PER_PAGE,
   })
-
-  const goToPreviousPage = () => router.push({ query: { page: page - 1 } })
-  const goToNextPage = () => router.push({ query: { page: page + 1 } })
 
   return (
     <div>
@@ -32,13 +31,6 @@ export const ProjectsList = () => {
           </li>
         ))}
       </ul>
-
-      <button disabled={page === 0} onClick={goToPreviousPage}>
-        Previous
-      </button>
-      <button disabled={!hasMore} onClick={goToNextPage}>
-        Next
-      </button>
     </div>
   )
 }
