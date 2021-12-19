8bc1430d33311252ca103c593c604236fa4e9303
Facundo Prieto <facundo.prieto321@gmail.com>
2021-12-19 02:27:42 -0300

    Update task form

--- app/tasks/components/TaskForm.tsx
+++ app/tasks/components/TaskForm.tsx
@@ -1,3 +1,4 @@
+import { InputLeftElement } from "@chakra-ui/react"
 import { Form, FormProps } from "app/core/components/Form"
 import { FormTextInput } from "app/core/components/Forms/FormTextInput"
 import { z } from "zod"
@@ -6,7 +7,11 @@ export { FORM_ERROR } from "app/core/components/Form"
 export function TaskForm<S extends z.ZodType<any, any>>(props: FormProps<S>) {
   return (
     <Form<S> {...props}>
-      <FormTextInput name="name" placeholder="Task" />
+      <FormTextInput variant="flushed" name="name" placeholder="Task">
+        <InputLeftElement pointerEvents="none" color="gray.300" fontSize="1.2em">
+          +
+        </InputLeftElement>
+      </FormTextInput>
     </Form>
   )
 }
