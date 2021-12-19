fe94558579a915ef1fc37906b623e839a64fe74f
Facundo Prieto <facundo.prieto321@gmail.com>
2021-12-19 02:28:53 -0300

    Update task item
    Checks working with db

--- app/tasks/components/TaskItem.tsx
+++ app/tasks/components/TaskItem.tsx
@@ -3,29 +3,50 @@ import { useMutation, useQuery } from "blitz"
 import React, { Suspense } from "react"
 import deleteTask from "../mutations/deleteTask"
 import getTask from "../queries/getTask"
+import updateTask from "app/tasks/mutations/updateTask"
+import { FORM_ERROR } from "app/tasks/components/TaskForm"
 
-export const TaskItem = ({ taskId, initialState = false }) => {
-  const [state, setState] = useBoolean(initialState)
-  const [task] = useQuery(getTask, { id: taskId })
-  // const [updateTaskMutation] = useMutation(updateTask)
+export const TaskItem = ({ taskId }) => {
+  const [task, { setQueryData }] = useQuery(getTask, { id: taskId })
+  const [updateTaskMutation] = useMutation(updateTask)
   const [deleteTaskMutation] = useMutation(deleteTask)
 
   if (!task) return null
 
+  async function handleOnCheck() {
+    try {
+      if (!task) return null
+      const updated = await updateTaskMutation({
+        ...task,
+        id: task?.id,
+        checked: !task.checked,
+      })
+      await setQueryData(updated)
+    } catch (error: any) {
+      console.error(error)
+      return {
+        [FORM_ERROR]: error.toString(),
+      }
+    }
+  }
+
   return (
     <HStack
-      border={"2px solid"}
-      borderColor="gray.100"
-      borderRadius={"md"}
-      boxShadow="sm"
-      _hover={{ boxShadow: "md", opacity: 1 }}
+      _hover={{ bg: "gray.100" }}
       spacing={0}
       w="full"
       cursor="pointer"
-      opacity={`${state ? 0.4 : 1}`}
+      opacity={`${task.checked ? 0.4 : 1}`}
       zIndex={0}
     >
-      <Checkbox zIndex={1} p="2" pr="0" isChecked={state} onChange={setState.toggle} />
+      <Checkbox
+        borderRadius="full"
+        zIndex={1}
+        p="2"
+        pr="0"
+        isChecked={task.checked}
+        onChange={handleOnCheck}
+      />
 
       <Suspense fallback={<SkeletonText p="2" />}>
         <Text w="full" onClick={() => alert("hello")} p="2">
