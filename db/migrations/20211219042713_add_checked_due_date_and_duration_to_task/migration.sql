0be4fbcf265bc8993e9714d569ed6081e229d027
Facundo Prieto <facundo.prieto321@gmail.com>
2021-12-19 02:26:39 -0300

    Update task db scheme

---
+++ db/migrations/20211219042713_add_checked_due_date_and_duration_to_task/migration.sql
@@ -0,0 +1,19 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Task" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "checked" BOOLEAN NOT NULL DEFAULT false,
+    "dueDate" DATETIME,
+    "duration" INTEGER,
+    "projectId" INTEGER,
+    CONSTRAINT "Task_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+INSERT INTO "new_Task" ("createdAt", "id", "name", "projectId", "updatedAt") SELECT "createdAt", "id", "name", "projectId", "updatedAt" FROM "Task";
+DROP TABLE "Task";
+ALTER TABLE "new_Task" RENAME TO "Task";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
